96d2ab46a6a21725eafe30af80911be9e42d25df
dmm <dmm@dmm.dmm>
2025-07-13 10:42:30 +0400

    ref

--- src/test/kotlin/gradle/dsl/GradleBuildDslBuilderTest.kt
+++ src/test/kotlin/gradle/dsl/GradleBuildDslBuilderTest.kt
@@ -228,9 +228,9 @@ class GradleBuildDslBuilderTest {
             |        mavenLocal()
             |    }
             |}
+            |val signingKeyId = ""
+            |val signingKey: String by project
             |signing {
-            |    val signingKeyId: String? by project
-            |    val signingKey: String? by project
             |    val signingPassword: String? by project
             |    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
             |    sign(publishing.publications)
